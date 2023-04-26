667ab59d7ef389b672423abec1bd9fc83db2a9b6
yuki <72589895+Ryuki-4648@users.noreply.github.com>
2023-04-27 08:02:05 +0900

    importの並び変更、コンポーネント化、linkタグの記述場所変更など

---
+++ pome/src/components/bottom.tsx
@@ -0,0 +1,11 @@
+export default function Bottom() {
+  return (
+    <div className="l-bottom">
+      <div className="l-bottom__link">
+        <a href="https://docs.google.com/forms/d/e/1FAIpQLSe5RJxhW1gDHo5JDDYZaLQrQS8Ip2kTloIIk4l9EhR2yrfHUA/viewform" target="_blank">お問い合わせ</a>
+        <a href="/privacy/">プライバシーポリシー</a>
+      </div>
+      <small className="c-text__copyright">&copy; 2023 ほめらにあん。 by R.YUKI Portfolio</small>
+    </div>
+  )
+}
