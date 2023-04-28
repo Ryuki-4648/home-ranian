a7ce48ea439062bc6c9865a0a7704c948571cfbb
yuki <72589895+Ryuki-4648@users.noreply.github.com>
2023-04-29 08:38:21 +0900

    eslintrc, next config設定変更

--- pome/next.config.js
+++ pome/next.config.js
@@ -1,5 +1,13 @@
+/* 公開時のサブディレクトリ */
+const SUB_DIRECTORY = "/home-ranian";
+
+/* 本番環境と開発環境の分岐用のフラグ */
+const isProd = process.env.NODE_ENV == "production"
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  assetPrefix: isProd ? SUB_DIRECTORY : "",
+  basePath: isProd ? SUB_DIRECTORY : "",
   reactStrictMode: true,
 }
 
